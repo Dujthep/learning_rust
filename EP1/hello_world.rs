ef660f3f8ab2a669953d73c11f9f4591330e16df
dujthep.stop <dujthep.stop@gmail.com>
2025-01-06 20:51:10 +0700

    DELETE DUPLICATE CODE

--- EP1/hello_world.rs
+++ EP1/hello_world.rs
@@ -3,14 +3,4 @@ fn main() {
     let married = true;
     println!("Hello, {}!", name);
     println!("I'm {}", if married { "married" } else { "single" });
-
-    let name = "STOP";
-    let married = true;
-    println!("Hello, {}!", name);
-    println!("I'm {}", if married { "married" } else { "single" });
-
-    let name = "STOP";
-    let married = true;
-    println!("Hello, {}!", name);
-    println!("I'm {}", if married { "married" } else { "single" });
 }
