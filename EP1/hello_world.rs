6112537b0c71c3fe52f8eb5cfade4edaf0d9ec09
dujthep.stop <dujthep.stop@gmail.com>
2025-01-06 20:49:55 +0700

    TEST COMMIT

--- EP1/hello_world.rs
+++ EP1/hello_world.rs
@@ -3,4 +3,14 @@ fn main() {
     let married = true;
     println!("Hello, {}!", name);
     println!("I'm {}", if married { "married" } else { "single" });
+
+    let name = "STOP";
+    let married = true;
+    println!("Hello, {}!", name);
+    println!("I'm {}", if married { "married" } else { "single" });
+
+    let name = "STOP";
+    let married = true;
+    println!("Hello, {}!", name);
+    println!("I'm {}", if married { "married" } else { "single" });
 }
